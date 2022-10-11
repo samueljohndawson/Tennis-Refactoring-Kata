43869210239aab61141e9534b9249df70355d2a0
Sam Dawson <Sam.Dawson@bjss.com>
2022-10-11 10:46:30 +0100

    made player objects in the constructor

--- javascript/TennisGame1.js
+++ javascript/TennisGame1.js
@@ -1,22 +1,27 @@
-var TennisGame1 = function(player1Name, player2Name) {
-    this.m_score1 = 0;
-    this.m_score2 = 0;
-    this.player1Name = player1Name;
-    this.player2Name = player2Name;
+var TennisGame1 = function (player1Name, player2Name) {
+    // this.player1score = 0;
+    // this.player2score = 0;
+    // this.player1Name = player1Name;
+    // this.player2Name = player2Name;
+
+    this.player1 = { name: player1Name, score: 0 }
+    this.player2 = { name: player2Name, score: 0 }
+
 };
 
-TennisGame1.prototype.wonPoint = function(playerName) {
+TennisGame1.prototype.wonPoint = function (playerName) {
     if (playerName === "player1")
-        this.m_score1 += 1;
+        this.player1.score += 1;
     else
-        this.m_score2 += 1;
+        this.player2.score += 1;
+
 };
 
-TennisGame1.prototype.getScore = function() {
+TennisGame1.prototype.getScore = function () {
     var score = "";
     var tempScore = 0;
-    if (this.m_score1 === this.m_score2) {
-        switch (this.m_score1) {
+    if (this.player1.score === this.player2.score) {
+        switch (this.player1.score) {
             case 0:
                 score = "Love-All";
                 break;
@@ -30,18 +35,18 @@ TennisGame1.prototype.getScore = function() {
                 score = "Deuce";
                 break;
         }
-    } else if (this.m_score1 >= 4 || this.m_score2 >= 4) {
-        var minusResult = this.m_score1 - this.m_score2;
+    } else if (this.player1.score >= 4 || this.player2.score >= 4) {
+        var minusResult = this.player1.score - this.player2.score;
         if (minusResult === 1) score = "Advantage player1";
         else if (minusResult === -1) score = "Advantage player2";
         else if (minusResult >= 2) score = "Win for player1";
         else score = "Win for player2";
     } else {
         for (var i = 1; i < 3; i++) {
-            if (i === 1) tempScore = this.m_score1;
+            if (i === 1) tempScore = this.player1.score;
             else {
                 score += "-";
-                tempScore = this.m_score2;
+                tempScore = this.player2.score;
             }
             switch (tempScore) {
                 case 0:
